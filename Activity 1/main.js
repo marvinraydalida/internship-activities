ff6896e4049c1f439224bcb46d0839d11e5e25ae
marvinraydalida <marvinraydalida@gmail.com>
2023-03-21 17:23:49 +0800

    Added responsive menu

--- Activity 1/main.js
+++ Activity 1/main.js
@@ -1,6 +1,9 @@
 const toggleSwitchContainer = document.getElementById("toggle-container");
 const toggleSwitch = document.getElementById("toggle-switch");
 const navButtons = document.getElementsByClassName("nav-button");
+const menuButton = document.querySelector('#menu-button i');
+const closeMenuButton = document.querySelector("#close-menu i");
+const sideMenu = document.getElementById("side-menu");
 
 toggleSwitchContainer.addEventListener("click", (event) => {
   event.target.classList.toggle("slide");
@@ -14,7 +17,9 @@ toggleSwitchContainer.addEventListener("click", (event) => {
     event.target.style.backgroundColor = "#E5E5E5";
     event.target.style.borderColor = "#B4B4B4";
   }
-  switchMode(navButtons)
+  switchMode(menuButton);
+  switchMode(navButtons);
+  switchMode(sideMenu);
   setTimeout(function () {
     event.target.children[0].classList.toggle("slide-animate");
   }, 350);
@@ -32,19 +37,32 @@ toggleSwitch.addEventListener("click", (event) => {
     event.target.parentNode.style.backgroundColor = "#E5E5E5";
     event.target.parentNode.style.borderColor = "#B4B4B4";
   }
-  switchMode(navButtons)
+  switchMode(menuButton);
+  switchMode(navButtons);
+  switchMode(sideMenu);
   setTimeout(function () {
     event.target.classList.toggle("slide-animate");
   }, 350);
 });
 
+menuButton.addEventListener("click", (event) => {
+    sideMenu.classList.toggle("side-menu-show");
+});
+
+closeMenuButton.addEventListener("click", (event) => {
+    sideMenu.classList.toggle("side-menu-show");
+});
+
 function switchMode(node){
+
     if(node.length !== undefined){
         for(let el of node){
             el.classList.toggle('light-font');
         }
     }
     else {
+        if(node.id === "side-menu") 
+            node.classList.toggle("light-side-menu");
         node.classList.toggle('light-font');
     }
 }
